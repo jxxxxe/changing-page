d8cbf1c097ec179b33263a1fc42bbd8818a7e30a
J <68155263+jxxxxe@users.noreply.github.com>
2023-12-21 00:07:22 +0900

    feat: 헤더 생성

---
+++ src/components/Header/BackButton.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const BackButton = styled.button`
+  border-radius: 100px;
+  border: 1px solid transparent;
+  font-size: 1em;
+  font-weight: 500;
+  background-color: transparent;
+  color: #3b3b3b;
+  cursor: pointer;
+  transition: border-color 0.25s;
+  margin: 5px;
+`;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const Header = styled.header`
+  height: 50px;
+  background-color: pink;
+  display: flex;
+  align-items: center;
+`;
