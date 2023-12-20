787fa5c12e0e5781397547dbe259d9f4d02eaf20
J <68155263+jxxxxe@users.noreply.github.com>
2023-12-21 00:09:43 +0900

    feat: App 컴포넌트에 띄움

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
-import "./App.css";
+import First from "./components/Page/Pages/FirstPage";
+import Second from "./components/Page/Pages/SecondPage";
+import Third from "./components/Page/Pages/ThirdPage";
+import styled from "@emotion/styled";
+import { Breadcrumb, Header, BackButton } from "./components/Header";
+import { usePageList } from "./provider/pageListProvider.tsx";
+
+const PageView = styled.div`
+  height: 100%;
+  background-color: white;
+  display: flex;
+`;
+
+const BreadcrumbContainer = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+`;
 
 function App() {
-  return <></>;
+  const { pageList, pushPage, popPage } = usePageList();
+
+  let currentPage = null;
+
+  switch (pageList[pageList.length - 1]) {
+    case "first":
+      currentPage = <First onClick={(page) => pushPage(page)} />;
+      break;
+    case "second":
+      currentPage = <Second onClick={(page) => pushPage(page)} />;
+      break;
+    case "third":
+      currentPage = <Third onClick={(page) => pushPage(page)} />;
+      break;
+  }
+
+  return (
+    <>
+      <Header>
+        {pageList.length > 1 && (
+          <BackButton onClick={() => popPage()}>Back</BackButton>
+        )}
+        <BreadcrumbContainer>
+          <Breadcrumb />
+        </BreadcrumbContainer>
+      </Header>
+      <PageView>{currentPage}</PageView>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { PageListProvider } from "./provider/pageListProvider";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <PageListProvider>
+    <App />
+  </PageListProvider>
+);
